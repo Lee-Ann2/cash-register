323e57d74522ae50fa8d1d23623439b98f5eca16
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-03-04 01:48:27 +0200

    Having a little bug. Fix the error in line 10. Change the string to an integer.

--- cash-register.py
+++ cash-register.py
@@ -4,7 +4,17 @@ def change(due, paid):
         
     denominations = {'R200': 20000, 'R100': 10000, 'R50': '5000', 'R20': 2000, 'R10': 1000, 'R5': 500, 'R2': 200, 'R1': 100, '50c': 50, '20c': 20, '10c': 10, '5c': 5}
     changeDictionary = {}
-    
+    changeLeft = paid - due
+
     for denomination, value in denominations.items():
-        if change >= value:
+        if changeLeft >= value:
             count = change // value
+            changeDictionary[denomination] = count
+            changeLeft -= count * value
+    return changeDictionary
+change(1222, 2000)
+due = 1222
+paid = 2000
+
+total = paid - due
+print(total)
