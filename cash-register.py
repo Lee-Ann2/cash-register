2736b852615ac30c71017f860a7b15d5a906a789
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-03-04 01:39:26 +0200

    Assigning an empty dictionary to changeDictionary.

--- cash-register.py
+++ cash-register.py
@@ -2,4 +2,9 @@ def change(due, paid):
     if paid < due:
         raise ValueError(f'amount paid should be greater than amount due')
         
-        denominations = {'R200': 20000, 'R100': 10000, 'R50': '5000', 'R20': 2000, 'R10': 1000, 'R5': 500, 'R2': 200, 'R1': 100, '50c': 50, '20c': 20, '10c': 10, '5c': 5}
+    denominations = {'R200': 20000, 'R100': 10000, 'R50': '5000', 'R20': 2000, 'R10': 1000, 'R5': 500, 'R2': 200, 'R1': 100, '50c': 50, '20c': 20, '10c': 10, '5c': 5}
+    changeDictionary = {}
+    
+    for denomination, value in denominations.items():
+        if change >= value:
+            count = change // value
