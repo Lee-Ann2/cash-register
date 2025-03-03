d0f692d1c3045f5616aab8f7c2eb1017a7aedfb5
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-03-04 01:34:08 +0200

    Added a dictionary for the amounts in cents.

--- cash-register.py
+++ cash-register.py
@@ -1,4 +1,5 @@
 def change(due, paid):
     if paid < due:
         raise ValueError(f'amount paid should be greater than amount due')
-    
+        
+        denominations = {'R200': 20000, 'R100': 10000, 'R50': '5000', 'R20': 2000, 'R10': 1000, 'R5': 500, 'R2': 200, 'R1': 100, '50c': 50, '20c': 20, '10c': 10, '5c': 5}
